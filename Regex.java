26952d708101adf7cabab92b0fb4bd64141f923a
Your Name <you@example.com>
2024-02-02 22:16:49 +0200

    added the file scanner

--- Regex.java
+++ Regex.java
@@ -1,5 +1,20 @@
 import java.io.*;
-import.java.util.*;
+import java.util.*;
 public class Regex {
-    
+    public static void main(String[] args) {
+        ArrayList<String> ipFile=new ArrayList<>();
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter file with IP-Address:");
+        String filename=scanner.nextLine();
+        try{File file=new File(filename);
+            scanner=new Scanner(file);
+            while (scanner.hasNextLine()) {
+                ipFile.add(scanner.nextLine());
+            }
+
+        }catch(FileNotFoundException e){
+            System.out.println("ASKIES file not found.");
+        }
+        
+    }
 }
