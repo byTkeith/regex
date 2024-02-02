ccc34ede38109f6e236496f004948fbbbf999594
Your Name <you@example.com>
2024-02-02 22:29:26 +0200

    split the values to char

--- Regex.java
+++ Regex.java
@@ -17,7 +17,14 @@ public class Regex {
         }
         for(String fileValues: ipFile){//this loop loops over the entire file line by line
             String[] eachIpUnit=fileValues.split(".");//splits each line by the "."
-            
+            if(eachIpUnit.length>4||eachIpUnit.length<4){//if not within the range of an ip address
+                System.out.println("length too long for an IP-Address.");
+                    
+            }
+            for(int i=0;i<eachIpUnit.length;i++){//loops through the split values if above 4 then its not an ip address
+                char[] unitLength=eachIpUnit[i].toCharArray();//splits each unit to char values
+                
+            }
         }
         
     }
