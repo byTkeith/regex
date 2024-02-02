64b2369ee3f0ec6e82ccdd10cb41e382e03e713a
Your Name <you@example.com>
2024-02-03 00:01:57 +0200

    sepated rhe values by comma

--- Regex.java
+++ Regex.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class Regex {
     public static void main(String[] args) {
         ArrayList<String> ipFile=new ArrayList<>();
+        ArrayList<Character> newList=new ArrayList<>();
         Scanner scanner=new Scanner(System.in);
         System.out.println("Enter file with IP-Address:");
         String filename=scanner.nextLine();
@@ -18,10 +19,20 @@ public class Regex {
         for(String fileValues: ipFile){//this loop loops over the entire file line by line
             char[] eachIpUnit=fileValues.toCharArray();//splits the words into char values
             int numOfCommas=0;
+            int numLength=0;
             for(int i=0;i<eachIpUnit.length;i++){
+                numLength=numLength+1;
                 if(eachIpUnit[i]=='.'){
-                    numOfCommas+=numOfCommas;
+                    numOfCommas=numOfCommas+1;
+                    
                 }
+
+            } if(numOfCommas<3||numOfCommas>3){
+                System.out.println("The IP-Address does not  saticifies for "+fileValues+" as lenght exceeds.");
+            }
+            else {
+                System.out.println("the value "+fileValues+" is correct");
+
 
             }
            
