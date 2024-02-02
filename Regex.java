21a87da40972ad7dc85160cb5dcf11cc0c86e3a7
Your Name <you@example.com>
2024-02-02 22:36:53 +0200

    added comparisons

--- Regex.java
+++ Regex.java
@@ -18,16 +18,20 @@ public class Regex {
         for(String fileValues: ipFile){//this loop loops over the entire file line by line
             String[] eachIpUnit=fileValues.split(".");//splits each line by the "."
             if(eachIpUnit.length>4||eachIpUnit.length<4){//if not within the range of an ip address
-                System.out.println("length too long for an IP-Address.");
+                System.out.println("length too long for an IP-Address: "+fileValues);
                     
             }
             for(int i=0;i<eachIpUnit.length;i++){//loops through the split values if above 4 then its not an ip address
                 char[] unitLength=eachIpUnit[i].toCharArray();//splits each unit to char values
                 int numValues=Integer.parseInt(eachIpUnit[i]);//converst each unit to a int value
-                if(unitLength.length>3||)
+                if(unitLength.length<=3||numValues<=255){
+                    System.out.println("Correct IP-Address: "+fileValues);
+
+                }
+                else{System.out.println("Wrong format IP-Address: "+fileValues);}
                 
             }
-        }
+        }System.out.println("Done.");
         
     }
 }
