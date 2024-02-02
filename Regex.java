f2785533aac2f8831bf1e3cedf45a39b94d61f92
Your Name <you@example.com>
2024-02-02 23:13:03 +0200

    count the number of commas

--- Regex.java
+++ Regex.java
@@ -17,24 +17,16 @@ public class Regex {
         }
         for(String fileValues: ipFile){//this loop loops over the entire file line by line
             char[] eachIpUnit=fileValues.toCharArray();//splits the words into char values
-            for(char w: eachIpUnit){
-                System.out.println("split values in eachIpUnit "+w);
-            }
-            //System.out.println("length of each fileValue "+eachIpUnit.);
-            // if(eachIpUnit.length>4||eachIpUnit.length<4){//if not within the range of an ip address
-            //     System.out.println("length too long for an IP-Address: "+fileValues);
-                    
-            // }
-            // for(int i=0;i<eachIpUnit.length;i++){//loops through the split values if above 4 then its not an ip address
-            //     char[] unitLength=eachIpUnit[i].toCharArray();//splits each unit to char values
-            //     int numValues=Integer.parseInt(eachIpUnit[i]);//converst each unit to a int value
-            //     if(unitLength.length<=3||numValues<=255){
-            //         System.out.println("Correct IP-Address: "+fileValues);
+            int numOfCommas=0;
+            for(int i=0;i<eachIpUnit.length;i++){
+                if(eachIpUnit[i]=='.'){
+                    numOfCommas+=numOfCommas;
+                }
 
-            //     }
-            //     else{System.out.println("Wrong format IP-Address: "+fileValues);}
-                
-            // }
+            }
+           
+           
+           
         }System.out.println("Done.");
 
         
